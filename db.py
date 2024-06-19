cc78963eeaac3896da5c674fe8882e21672c202c
Trane1235 <Trane1235@yandex.ru>
2024-06-19 23:38:46 +0400

    тблица sql 23 38

--- db.py
+++ db.py
@@ -1,9 +1,11 @@
 import sqlite3 as sq
-with sq.connect('sapper3.db') as con:
+with sq.connect('sappe3.db') as con:
     cur = con.cursor()
-    cur.execute("""CREATE TABLE useres(
-    name TEXT,
-    sex INTEGER,
+    cur.execute('''DROP TABLE IF EXISTS useres''')
+    cur.execute("""CREATE TABLE IF NOT EXISTS useres(
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    sex INTEGER DEFAULT 1 ,
     old INTEGER,
     score INTEGER
     )""")
